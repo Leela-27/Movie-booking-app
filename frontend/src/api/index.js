c8d1316468fae1e0e86bfb91534eb51477aac972
leela-27 <leelasanthosh27@gmai.com>
2024-06-18 10:49:13 +0530

    small change

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 const url = 'https://movie-booking-app-wavz.onrender.com'
 // const url = 'http://localhost:8080'
 
-export const fetchLastBookingDetails = () => axios.get(`${url}/api/last`)
+export const fetchLastBookingDetails = async () =>  await axios.get(`${url}/api/last`)
 
-export const sendingBookingDetails = (bookingData) => axios.post(`${url}/api/bookings`, bookingData)
+export const sendingBookingDetails = async (bookingData) => await axios.post(`${url}/api/bookings`, bookingData)
